76872e7f3a2aadda995936f2bdd55b7b8ac8f5ad
ilies1511 <iliesziane@hotmail.com>
2024-05-13 19:44:00 +0200

    Check Taregt Position

--- list_list/list.c
+++ list_list/list.c
@@ -6,7 +6,7 @@
 /*   By: ilies1511 <ilies1511@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/04 23:04:10 by iziane            #+#    #+#             */
-/*   Updated: 2024/05/13 17:37:10 by ilies1511        ###   ########.fr       */
+/*   Updated: 2024/05/13 19:42:10 by ilies1511        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,11 +94,35 @@ void	list_manager(int *array, int amount_numbers, int *sorted_array)
 	printf("Amount Nodes: %d\n", count_node(&tail_a));
 	current_position(&tail_a);
 	current_position(&tail_b);
-	sa(&tail_a, 1);
+	// sa(&tail_a, 1);
 	// rra(&tail_a, 1);
 	// rra(&tail_a, 1);
 	// rrb(&tail_b, 1);
 	// ra(&tail_a, 1);
+	current = tail_a;
+	printf("\nPrint array of stack a before p2b: \n");
+	while (1 && tail_a)
+	{
+		printf("value: %d index: %d current pos: %d\n", current->x, current->index, current->pos);
+		current = current->next;
+		if (current == tail_a)
+			break ;
+	}
+	p2b(&tail_a, &tail_b);
+	// pb(&tail_a, &tail_b);
+	// pb(&tail_a, &tail_b);
+	// pb(&tail_a, &tail_b);
+	printf("\nPrint array of stack b: \n");
+	current = tail_b;
+	while (1 && tail_b)
+	{
+		printf("value: %d index: %d current pos: %d\n", current->x, current->index, current->pos);
+		current = current->next;
+		if (current == tail_b)
+			break ;
+	}
+
+	printf("\nPrint array of stack a: \n");
 	current = tail_a;
 	while (1 && tail_a)
 	{
@@ -107,29 +131,6 @@ void	list_manager(int *array, int amount_numbers, int *sorted_array)
 		if (current == tail_a)
 			break ;
 	}
-	// p2b(&tail_a, &tail_b);
-	// pb(&tail_a, &tail_b);
-	// pb(&tail_a, &tail_b);
-	// pb(&tail_a, &tail_b);
-	// printf("\nPrint array of stack b: \n");
-	// current = tail_b;
-	// while (1 && tail_b)
-	// {
-	// 	printf("value: %d index: %d current pos: %d\n", current->x, current->index, current->pos);
-	// 	current = current->next;
-	// 	if (current == tail_b)
-	// 		break ;
-	// }
-
-	// printf("\nPrint array of stack a: \n");
-	// current = tail_a;
-	// while (1 && tail_a)
-	// {
-	// 	printf("value: %d index: %d current pos: %d\n", current->x, current->index, current->pos);
-	// 	current = current->next;
-	// 	if (current == tail_a)
-	// 		break ;
-	// }
 	// pa(&tail_a, &tail_b);
 	// printf("\nPrint array of stack a after pa: \n");
 	// current = tail_a;
@@ -140,22 +141,24 @@ void	list_manager(int *array, int amount_numbers, int *sorted_array)
 	// 	if (current == tail_a)
 	// 		break ;
 	// }
-	// find_target_pos(&tail_a, &tail_b);
-	// current = tail_b;
-	// while (1 && tail_b)
-	// {
-	// 	printf("value: %d index: %d current pos: %d target_pos: %d\n", current->x, current->index, current->pos, current->target_pos);
-	// 	current = current->next;
-	// 	if (current == tail_b)
-	// 		break ;
-	// }
-	// printf("\nPrint array of stack a: \n");
-	// current = tail_a;
-	// while (1 && tail_a)
-	// {
-	// 	printf("value: %d index: %d current pos: %d\n", current->x, current->index, current->pos);
-	// 	current = current->next;
-	// 	if (current == tail_a)
-	// 		break ;
-	// }
+	current_position(&tail_a);
+	current_position(&tail_b);
+	find_target_pos(&tail_a, &tail_b);
+	current = tail_b;
+	while (1 && tail_b)
+	{
+		printf("value: %d index: %d current pos: %d target_pos: %d\n", current->x, current->index, current->pos, current->target_pos);
+		current = current->next;
+		if (current == tail_b)
+			break ;
+	}
+	printf("\nPrint array of stack a: \n");
+	current = tail_a;
+	while (1 && tail_a)
+	{
+		printf("value: %d index: %d current pos: %d\n", current->x, current->index, current->pos);
+		current = current->next;
+		if (current == tail_a)
+			break ;
+	}
 }
